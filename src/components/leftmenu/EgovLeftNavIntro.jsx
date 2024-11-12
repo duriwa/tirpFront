78d659449f7b863fe97c6dbc3110f8be0106e014
하나생명 <hanalife00@gmail.com>
2024-11-12 18:04:39 +0900

    정기반영목록

--- src/components/leftmenu/EgovLeftNavIntro.jsx
+++ src/components/leftmenu/EgovLeftNavIntro.jsx
@@ -4,18 +4,39 @@ import { NavLink } from 'react-router-dom';
 import URL from 'constants/url';
 
 function EgovLeftNavIntro() {
-    
-    return (
-        <div className="nav">
-            <div className="inner">
-                <h2>정보마당</h2>
-                <ul className="menu4">
-                    <li><NavLink to={URL.INTRO_WORKS} className={({ isActive }) => (isActive ? "cur" : "")}>주요사업 소개</NavLink></li>
-                    <li><NavLink to={URL.INTRO_SERVICE} className={({ isActive }) => (isActive ? "cur" : "")}>대표서비스 소개</NavLink></li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className='nav'>
+      <div className='inner'>
+        <h2>주간보고</h2>
+        <ul className='menu4'>
+          <li>
+            <NavLink
+              to={URL.INTRO_WORKS}
+              className={({ isActive }) => (isActive ? 'cur' : '')}
+            >
+              주간보고
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={URL.INTRO_SERVICE}
+              className={({ isActive }) => (isActive ? 'cur' : '')}
+            >
+              기타(교육, 휴가)
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={URL.INTRO_SERCH_DOWN}
+              className={({ isActive }) => (isActive ? 'cur' : '')}
+            >
+              전체 조회 및 다운로드
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default EgovLeftNavIntro;
