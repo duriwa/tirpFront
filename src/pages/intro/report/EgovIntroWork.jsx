08937d8fd0adfe77890798da5ec4e592a564e29c
하나생명 <hanalife00@gmail.com>
2024-11-18 15:17:45 +0900

    주간보고!!

--- src/pages/intro/report/EgovIntroWork.jsx
+++ src/pages/intro/report/EgovIntroWork.jsx
@@ -111,7 +111,7 @@ function EgovIntroWork() {
 
   useEffect(function () {
     //initMode();
-    // eslint-disable-next-line react-hooks/exhaustive-deps ddd
+    // eslint-disable-next-line react-hooks/exhaustive-deps dddㅇㅇㅇㅇ
   }, []);
 
   console.log('------------------------------EgovAdminScheduleEdit [End]');
